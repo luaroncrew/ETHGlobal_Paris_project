899f0ab8c1f686e789c01b98672d72df94bdcbe0
luaroncrew <ma-kir.ru@yandex.ru>
2023-07-22 20:07:49 +0200

    repair cors, call locally installed cartesi for calculations

--- basic_rate_observation_frontend/src/app/app.component.ts
+++ basic_rate_observation_frontend/src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
-import {MatTabsModule} from '@angular/material/tabs';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+
+function hex2a(hexx: string) {
+  var hex = hexx.toString();
+  var str = '';
+  for (var i = 0; i < hex.length; i += 2)
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  return str;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -9,11 +21,23 @@ import {MatTabsModule} from '@angular/material/tabs';
 export class AppComponent {
   title = 'basicObservationFrontend';
   rate: boolean = false
+  inspectURL: string = 'http://localhost:5005/inspect/'
+  statistic: string = ""
 
+  constructor() {
+  }
   observeTab() {
     this.rate = false
   }
   rateTab() {
     this.rate = true
   }
+
+  async calculate_statistic(data_piece: string) {
+    const url: string = this.inspectURL;
+    const response = await fetch(url);
+    const response_contents = await response.json();
+    console.log(hex2a(response_contents['reports'][0]['payload']));
+  }
+
 }
