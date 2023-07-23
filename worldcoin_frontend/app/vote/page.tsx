2f0fe37f92f87b2beaf21bcd6f7af1ffaa5f7a02
Gwénolé MARTIN <Gwenole.Martin@grenoble-inp.org>
2023-07-23 03:08:25 +0200

    [FEAT] some frontend stuff

---
+++ worldcoin_frontend/app/vote/page.tsx
@@ -0,0 +1,7 @@
+export default function Vote() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="text-4xl font-bold">Vote</h1>
+        </main>
+    )
+}
